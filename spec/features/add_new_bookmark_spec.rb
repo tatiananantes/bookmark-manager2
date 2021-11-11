ea829c4e507b2a13aaba0d3144ef20a30b3111cc
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-11 19:21:45 +0000

    fix typo that was blocking tests on previous commit

--- spec/features/add_new_bookmark_spec.rb
+++ spec/features/add_new_bookmark_spec.rb
@@ -4,6 +4,7 @@ feature 'Adding a new  bookmark' do
         fill_in('url', with: 'http://www.bbc.co.uk')
         fill_in('title', with: 'BBC')
         click_button('Submit')
-        expect(page).to have_link ('BBC', href: 'http://www.bbc.co.uk')
+
+        expect(page).to have_link('BBC', href: 'http://www.bbc.co.uk')
     end 
 end 
